10ea2ed0d53d1e699c770a7a4b24298fc5fb59d9
Clsrfish <clsrfish@gmail.com>
2021-01-11 23:09:11 +0800

    opt<17>: optimize cost

--- src/0017.cpp
+++ src/0017.cpp
@@ -10,52 +10,49 @@
  */
 
 #include <iostream>
-#include <map>
 #include <vector>
 namespace leetcode_0017 {
 std::string key = "0017";
 class Solution {
    private:
-    std::vector<std::vector<char>> mapping = {
-        {'a', 'b', 'c'},
-        {'d', 'e', 'f'},
-        {'g', 'h', 'i'},
-        {'j', 'k', 'l'},
-        {'m', 'n', 'o'},
-        {'p', 'q', 'r', 's'},
-        {'t', 'u', 'v'},
-        {'w', 'x', 'y', 'z'},
-    };
-
-    // replace 2dimension vector with vector<int> to save mem
+    static std::vector<std::vector<char>> mapping;
 
    public:
     std::vector<std::string> letterCombinations(const std::string& digits) {
         std::vector<std::string> result;
-        if(digits.length() == 0){
+        if (digits.length() == 0) {
             return result;
         }
-        std::vector<char> path;
+        std::string path;
         backtrace(digits, path, result);
         return result;
     }
 
-    void backtrace(const std::string& digits, std::vector<char>& path, std::vector<std::string>& result) {
-        size_t index = path.size();
-        if (index == digits.size()) {
-            path.push_back('\0');
-            result.push_back(std::string(path.data()));
-            path.pop_back();
+    void backtrace(const std::string& digits, std::string& path, std::vector<std::string>& result) {
+        size_t index = path.length();
+        if (index == digits.length()) {
+            result.push_back(path);
             return;
         }
 
         std::vector<char>& choices = mapping[digits[index] - '2'];
-        for (size_t i = 0; i < choices.size(); i++) {
-            path.push_back(choices[i]);
+        for (auto&& c : choices) {  // auto&& avoid memory copying
+            path.push_back(c);
             backtrace(digits, path, result);
             path.pop_back();
         }
     }
+};
+
+std::vector<std::vector<char>> Solution::mapping = {
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'},
+    {'g', 'h', 'i'},
+    {'j', 'k', 'l'},
+    {'m', 'n', 'o'},
+    {'p', 'q', 'r', 's'},
+    {'t', 'u', 'v'},
+    {'w', 'x', 'y', 'z'},
 };
 
 int main() {
