9093553e57c4111c2138fd88bdbaf0fa3ad099a1
liuxin.fish.one <liuxin.fish.one@bytedance.com>
2021-05-13 13:03:14 +0800

    104. maximum depth of binary tree

---
+++ test/104_test.cpp
@@ -0,0 +1,39 @@
+// Unit tests for LeetCode Solution 104
+#include "../src/104.cpp"
+#include "./utils/tree_util.hpp"
+#include <gtest/gtest.h>
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace {
+class LeetCode104 : public testing::TestWithParam<std::tuple<std::vector<int>, int>> {
+protected:
+  leetcode_104::Solution solution;
+  LeetCode104() {
+  }
+  ~LeetCode104() override {
+  }
+  void SetUp() override {
+    std::vector<int> nodes;
+    std::tie(nodes, expected) = GetParam();
+    root = createBTree(nodes);
+  }
+  void TearDown() override {
+    freeTreeNodes(root);
+  }
+  TreeNode *root;
+  int expected;
+};
+
+// test_suite_name, test_name
+TEST_P(LeetCode104, Test) {
+  int actual = solution.maxDepth(root);
+  ASSERT_EQ(actual, expected);
+}
+
+// prefix, test_suite_name
+INSTANTIATE_TEST_SUITE_P(
+    P, LeetCode104,
+    testing::Values(std::make_tuple(std::vector<int>{3, 9, 20, NULL_NODE_VAL, NULL_NODE_VAL, 15, 7}, 3)));
+} // namespace
