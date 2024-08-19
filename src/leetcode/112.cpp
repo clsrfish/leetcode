d9f4ffd2549ddc8b54b12698db60e650afff2a4c
Clsrfish <clsrfish@gmail.com>
2024-08-19 20:54:55 +0800

    leetcode 112

--- src/leetcode/112.cpp
+++ src/leetcode/112.cpp
@@ -1,11 +1,11 @@
 /**
  * @file 112.cpp
  * @author clsrfish (clsrfish@gmail.com)
- * @brief  路径总和, https://leetcode-cn.com/problems/path-sum
+ * @brief  路径总和, https://leetcode.cn/problems/path-sum
  * @version 0.1
- * @date 2021-09-12
+ * @date 2024-08-19
  *
- * @copyright Copyright (c) 2021
+ * @copyright Copyright (c) 2024
  *
  */
 
@@ -22,31 +22,13 @@ class Solution {
     if (root == nullptr) {
       return false;
     }
-    return hasPathSum(root, targetSum, 0);
-  }
 
- private:
-  bool hasPathSum(TreeNode* root, int targetSum, int currentSum) {
-    if (root == nullptr) {
-      return targetSum == currentSum;
+    targetSum -= root->val;
+    if (root->left == nullptr && root->right == nullptr) {
+      return targetSum == 0;
     }
-    int newCurrenSum = currentSum + root->val;
-    if (root->left == nullptr && root->right == nullptr && newCurrenSum == targetSum) {
-      return true;
-    }
-    if (root->left != nullptr) {
-      bool hasPath = hasPathSum(root->left, targetSum, newCurrenSum);
-      if (hasPath) {
-        return true;
-      }
-    }
-    if (root->right != nullptr) {
-      bool hasPath = hasPathSum(root->right, targetSum, newCurrenSum);
-      if (hasPath) {
-        return true;
-      }
-    }
-    return false;
+
+    return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
   }
 };
 }  // namespace leetcode_112
