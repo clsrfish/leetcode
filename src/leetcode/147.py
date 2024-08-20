aac776ecb14f48fe79faeced30c33425df8a8a89
Clsrfish <clsrfish@gmail.com>
2024-08-20 23:38:53 +0800

    WIP

---
+++ src/leetcode/147.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from src.model import ListNode
+
+
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        return None
