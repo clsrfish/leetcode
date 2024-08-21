35078a964ed54238150ef85e67eee06eea9ee59a
Clsrfish <clsrfish@gmail.com>
2024-08-21 21:16:18 +0800

    show fixtures

--- src/model/ListNode.py
+++ src/model/ListNode.py
@@ -1,5 +1,28 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
-    def __init__(self, val=0, next=None):
+
+    def __init__(self, val=0, next: Optional["ListNode"] = None):
         self.val = val
         self.next = next
+
+    @staticmethod
+    def fromList(source: list[int]) -> "ListNode":
+        head = ListNode()
+        tail = head
+        for e in source:
+            tail.next = ListNode(e)
+            tail = tail.next
+
+        return head.next
+
+    @staticmethod
+    def toList(head: "ListNode") -> list[int]:
+        result = list()
+        while head is not None:
+            result.append(head.val)
+            head = head.next
+
+        return result
