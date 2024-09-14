556c4c637adef498115f5c4104494cb4c9c95871
Clsrfish <clsrfish@gmail.com>
2024-09-14 12:27:06 +0000

    239

--- src/leetcode/s239.py
+++ src/leetcode/s239.py
@@ -27,3 +27,32 @@ class Solution:
             right += 1
 
         return result
+
+
+class SolutionON:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # if k == 1:
+        #     return nums
+        result = list[int]()
+        maxValPos = list[int]()
+        left, right = 0, k - 1
+        while right < len(nums):
+            if len(maxValPos) > 0 and maxValPos[0] < left:
+                maxValPos.pop(0)
+
+            if len(maxValPos) == 0:
+                for i in range(left, right + 1):
+                    if len(maxValPos) == 0:
+                        maxValPos.append(i)
+                        continue
+                    while len(maxValPos) > 0 and nums[maxValPos[-1]] <= nums[i]:
+                        maxValPos.pop()
+                    maxValPos.append(i)
+            else:
+                while len(maxValPos) > 0 and nums[right] >= nums[maxValPos[-1]]:
+                    maxValPos.pop()
+                maxValPos.append(right)
+            result.append(nums[maxValPos[0]])
+            left += 1
+            right += 1
+        return result
--- tests/leetcode/239_test.py
+++ tests/leetcode/239_test.py
@@ -1,14 +1,21 @@
 import pytest
 
-from src.leetcode.s239 import Solution
+from src.leetcode.s239 import Solution, SolutionON
 
 dataset = lambda: [
     ([1, 3, -1, -3, 5, 3, 6, 7], 3, [3, 3, 5, 5, 6, 7]),
     ([1], 1, [1]),
+    ([1, -1], 1, [1, -1]),
 ]
 
 
 @pytest.mark.parametrize("nums,k,expected", dataset())
 def test(nums: list[int], k: int, expected: list[int]):
     result = Solution().maxSlidingWindow(nums, k)
+    assert result == expected
+
+
+@pytest.mark.parametrize("nums,k,expected", dataset())
+def test_on(nums: list[int], k: int, expected: list[int]):
+    result = SolutionON().maxSlidingWindow(nums, k)
     assert result == expected
