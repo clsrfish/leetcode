e99809c8f0da9e208fc77ca281d7599e15c6272b
Clsrfish <clsrfish@gmail.com>
2024-11-17 12:32:42 +0000

    40 update

--- src/leetcode/s40.py
+++ src/leetcode/s40.py
@@ -5,11 +5,11 @@ from src.model import ListNode, TreeNode
 
 class Solution:
     def __init__(self) -> None:
-        self.result = set[str]()
+        self.result = list[list[int]]()
 
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         self.combinationSum2Backtrace(sorted(candidates), target, 0, list[int]())
-        return [list(map(lambda n: int(n), x[1:-1].split(","))) for x in self.result]
+        return self.result
 
     def combinationSum2Backtrace(
         self, candidates: list[int], target: int, start: int, arr: list[int]
@@ -22,7 +22,7 @@ class Solution:
                 continue
             arr.append(n)
             if n == target:
-                self.result.add(str(arr))
+                self.result.append(arr[:])
             elif n <= target // 2:
                 self.combinationSum2Backtrace(candidates, target - n, i + 1, arr)
 
