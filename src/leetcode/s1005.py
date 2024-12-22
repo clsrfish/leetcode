a14806754a51ad373327020c7fafbfddb1fa65ee
Clsrfish <clsrfish@gmail.com>
2024-12-22 03:22:50 +0000

    1005 opt

--- src/leetcode/s1005.py
+++ src/leetcode/s1005.py
@@ -6,10 +6,13 @@ class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
         nums = sorted(nums)
         i = 0
-        for _ in range(0, k):
+        while k > 0:
+            k -= 1
             nums[i] = -nums[i]
 
             if i + 1 < len(nums) and nums[i] > nums[i + 1]:
                 i += 1
 
+            if nums[i] >= 0:
+                k = k % 2
         return sum(nums)
